4cd9294c35e4105cdef49c3b8116991e6adb42a5
ARKTEEK <minde398@gmail.com>
2024-04-16 18:00:49 +0300

    Implemented view routing

--- app/src/main/java/me/arkteek/worsetagram/MainActivity.kt
+++ app/src/main/java/me/arkteek/worsetagram/MainActivity.kt
@@ -6,28 +6,26 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import me.arkteek.worsetagram.ui.Header
-import me.arkteek.worsetagram.ui.Home
+import androidx.navigation.compose.rememberNavController
+import me.arkteek.worsetagram.routes.Router
 import me.arkteek.worsetagram.ui.theme.InstagramCloneTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
+      val navController = rememberNavController()
+
+      fun changePage(route: String) {
+        navController.navigate(route)
+      }
+
       InstagramCloneTheme {
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Home()
+          Router(navController, ::changePage)
         }
       }
     }
   }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-  InstagramCloneTheme { Header() }
 }
