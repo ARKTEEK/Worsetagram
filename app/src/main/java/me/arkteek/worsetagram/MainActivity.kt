4ca129dbc277fdfeaeb47acd604f16101b3df87c
ARKTEEK <minde398@gmail.com>
2024-05-11 23:16:12 +0300

    Dagger hilt implementation

--- app/src/main/java/me/arkteek/worsetagram/MainActivity.kt
+++ app/src/main/java/me/arkteek/worsetagram/MainActivity.kt
@@ -3,27 +3,29 @@ package me.arkteek.worsetagram
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
-import me.arkteek.worsetagram.routes.Router
-import me.arkteek.worsetagram.ui.theme.InstagramCloneTheme
+import dagger.hilt.android.AndroidEntryPoint
+import me.arkteek.worsetagram.ui.AppNavHost
+import me.arkteek.worsetagram.ui.theme.WorsetagramTheme
+import me.arkteek.worsetagram.ui.viewModel.AuthViewModel
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+  private val authViewModel by viewModels<AuthViewModel>()
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      val navController = rememberNavController()
-
-      fun changePage(route: String) {
-        navController.navigate(route)
-      }
-
-      InstagramCloneTheme {
+      WorsetagramTheme {
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Router(navController, ::changePage)
+          AppNavHost(authViewModel)
         }
       }
     }
