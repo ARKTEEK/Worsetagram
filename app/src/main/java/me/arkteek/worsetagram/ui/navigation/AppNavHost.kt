c6b5b358e0e459fb57258b7fb20cd6d5f15aca56
ARKTEEK <minde398@gmail.com>
2024-06-18 18:04:58 +0300

    Updated routes

--- app/src/main/java/me/arkteek/worsetagram/ui/navigation/AppNavHost.kt
+++ app/src/main/java/me/arkteek/worsetagram/ui/navigation/AppNavHost.kt
@@ -9,23 +9,28 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import me.arkteek.worsetagram.common.constants.ROUTE_APP_SETTINGS
 import me.arkteek.worsetagram.common.constants.ROUTE_CHAT
 import me.arkteek.worsetagram.common.constants.ROUTE_HOME
 import me.arkteek.worsetagram.common.constants.ROUTE_LOGIN
 import me.arkteek.worsetagram.common.constants.ROUTE_POST
 import me.arkteek.worsetagram.common.constants.ROUTE_PROFILE
 import me.arkteek.worsetagram.common.constants.ROUTE_PROFILE_OTHER
+import me.arkteek.worsetagram.common.constants.ROUTE_PROFILE_SETTINGS
 import me.arkteek.worsetagram.common.constants.ROUTE_SEARCH
 import me.arkteek.worsetagram.common.constants.ROUTE_SIGNUP
+import me.arkteek.worsetagram.ui.screen.AppSettingsScreen
 import me.arkteek.worsetagram.ui.screen.HomeScreen
+import me.arkteek.worsetagram.ui.screen.NewPostScreen
 import me.arkteek.worsetagram.ui.screen.SearchScreen
 import me.arkteek.worsetagram.ui.screen.authentication.AuthViewModel
 import me.arkteek.worsetagram.ui.screen.authentication.LoginScreen
 import me.arkteek.worsetagram.ui.screen.authentication.SignupScreen
 import me.arkteek.worsetagram.ui.screen.chat.ChatListScreen
-import me.arkteek.worsetagram.ui.screen.profile.NewPostScreen
 import me.arkteek.worsetagram.ui.screen.profile.ProfileOtherScreen
+import me.arkteek.worsetagram.ui.screen.profile.ProfileOtherViewModel
 import me.arkteek.worsetagram.ui.screen.profile.ProfileSelfScreen
+import me.arkteek.worsetagram.ui.screen.profile.ProfileSettingsScreen
 import me.arkteek.worsetagram.ui.screen.profile.ProfileViewModel
 
 @Composable
@@ -42,6 +47,11 @@ fun AppNavHost(
     composable(ROUTE_SEARCH) { SearchScreen(viewModel, navController) }
     composable(ROUTE_CHAT) { ChatListScreen(viewModel, navController) }
     composable(ROUTE_POST) { NewPostScreen(viewModel, navController) }
+    composable(ROUTE_APP_SETTINGS) { AppSettingsScreen(navController) }
+    composable(ROUTE_PROFILE_SETTINGS) {
+      val profileViewModel: ProfileViewModel = hiltViewModel()
+      ProfileSettingsScreen(viewModel = profileViewModel, navController)
+    }
     composable(ROUTE_PROFILE) {
       val profileViewModel: ProfileViewModel = hiltViewModel()
       ProfileSelfScreen(viewModel = profileViewModel, navController)
@@ -51,7 +61,7 @@ fun AppNavHost(
       arguments = listOf(navArgument("profileId") { type = NavType.StringType }),
     ) { entry ->
       val profileId = entry.arguments?.getString("profileId") ?: ""
-      val profileViewModel: ProfileViewModel = hiltViewModel()
+      val profileViewModel: ProfileOtherViewModel = hiltViewModel()
       ProfileOtherScreen(viewModel = profileViewModel, navController, profileId)
     }
   }
