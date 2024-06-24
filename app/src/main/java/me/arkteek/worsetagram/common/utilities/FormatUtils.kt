4ee00cec53c5af3a01ba4ce51f73a877e1440940
ARKTEEK <minde398@gmail.com>
2024-06-24 16:16:46 +0300

    Updated FormatUtils
    Depending on the number of days/hours/minutes, it will properly show day/days, hour/hours or minute/minutes.

--- app/src/main/java/me/arkteek/worsetagram/common/utilities/FormatUtils.kt
+++ app/src/main/java/me/arkteek/worsetagram/common/utilities/FormatUtils.kt
@@ -6,7 +6,6 @@ fun getTimeDifference(postTimestamp: Long): String {
   val currentTimeMillis = System.currentTimeMillis()
   val diffMillis = currentTimeMillis - postTimestamp
 
-  val seconds = TimeUnit.MILLISECONDS.toSeconds(diffMillis)
   val minutes = TimeUnit.MILLISECONDS.toMinutes(diffMillis)
   val hours = TimeUnit.MILLISECONDS.toHours(diffMillis)
   val days = TimeUnit.MILLISECONDS.toDays(diffMillis)
@@ -14,11 +13,11 @@ fun getTimeDifference(postTimestamp: Long): String {
   return when {
     days > 31 -> {
       val months = days / 30
-      "$months month(s) ago"
+      if (months == 1L) "$months month ago" else "$months months ago"
     }
-    days > 0 -> "$days day(s) ago"
-    hours > 0 -> "$hours hour(s) ago"
-    minutes > 0 -> "$minutes minute(s) ago"
+    days > 0 -> if (days == 1L) "$days day ago" else "$days days ago"
+    hours > 0 -> if (hours == 1L) "$hours hour ago" else "$hours hours ago"
+    minutes > 0 -> if (minutes == 1L) "$minutes minute ago" else "$minutes minutes ago"
     else -> "Just now"
   }
 }
